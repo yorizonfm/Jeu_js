118a82c910f3b3bfbb7d8758524794e65478cd45
YORIZON <yoan.ba@hotmail.fr>
2020-01-15 22:07:30 +0100

    commentaires

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");//on ecoute tout les bouttons
 
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(){
+    buttons[i].addEventListener("click", function(){ // on ecoute les clics aux bouttons
         
-        const joueur = buttons[i].innerHTML;
-        const robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
+        const joueur = buttons[i].innerHTML;//On va chercher le texte à l'interieure du boutton
+        const robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;//On va chercher un nombre aux hazar entre 0 et 1 multiplié par la taille du "tableau" et l'arrondire 
         let result= "";
         if (joueur === robot) {
             result = "Egalité";
